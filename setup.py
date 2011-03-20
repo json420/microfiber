d3fe1ff2767094d16424107dc98b7544bd8ab175
Jason Gerard DeRose <jderose@novacut.com>
2011-03-19 20:08:08 -0600

    License is LGPLv3+

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 
+# Authors:
+#   Jason Gerard DeRose <jderose@novacut.com>
+#
+# microfiber: fabric for a lightweight Couch
+# Copyright (C) 2011 Jason Gerard DeRose <jderose@novacut.com>
+#
+# This file is part of `microfiber`.
+#
+# `microfiber` is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# `microfiber` is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with `microfiber`.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Install `microfiber`.
 """
 
-import sys
 from distutils.core import setup
 from distutils.cmd import Command
 from unittest import TestLoader, TextTestRunner
@@ -38,7 +58,7 @@ class Test(Command):
         runner = TextTestRunner(verbosity=2)
         result = runner.run(suite)
         if not result.wasSuccessful():
-            sys.exit(1)
+            raise SystemExit(2)
 
 
 setup(
@@ -48,7 +68,7 @@ setup(
     version=microfiber.__version__,
     author='Jason Gerard DeRose',
     author_email='jderose@novacut.com',
-    license='PSF',
+    license='LGPLv3+',
     py_modules=['microfiber'],
     cmdclass={'test': Test},
 )
--- test_microfiber.py
+++ test_microfiber.py
@@ -1,3 +1,24 @@
+# Authors:
+#   Jason Gerard DeRose <jderose@novacut.com>
+#
+# microfiber: fabric for a lightweight Couch
+# Copyright (C) 2011 Jason Gerard DeRose <jderose@novacut.com>
+#
+# This file is part of `microfiber`.
+#
+# `microfiber` is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# `microfiber` is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with `microfiber`.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Unit tests for `microfiber` module.
 """
