df245b6957a0b64fa3027bc702f6c431d4bab741
Jason Gerard DeRose <jderose@novacut.com>
2011-03-25 02:31:56 -0600

    Benchmarks now do GET and DELETE as well

--- benchmark_microfiber.py
+++ benchmark_microfiber.py
@@ -25,19 +25,40 @@ def random_id():
 master = dict(
     ('a' * i, 'b' * i) for i in range(1, 50)
 )
+ids = tuple(random_id() for i in range(count))
+docs = []
 
 print('*** Benchmarking microfiber ***')
 print('Python: {}, {}, {}'.format(
     platform.python_version(), platform.machine(), platform.system())
 )
+
 print('Saving {} documents in db {!r}'.format(count, name))
 start = time.time()
-for i in range(count):
+for _id in ids:
     doc = dict(master)
-    doc['_id'] = random_id()
+    doc['_id'] = _id
     db.save(doc)
-
+    docs.append(doc)
 elapsed = time.time() - start
 print('Seconds: {:.2f}'.format(elapsed))
 print('Saves per second: {:.1f}'.format(count / elapsed))
+print('')
+
+print('Getting {} documents from db {!r}'.format(count, name))
+start = time.time()
+for _id in ids:
+    db.get(_id)
+elapsed = time.time() - start
+print('Seconds: {:.2f}'.format(elapsed))
+print('Gets per second: {:.1f}'.format(count / elapsed))
+print('')
+
+print('Deleting {} documents from db {!r}'.format(count, name))
+start = time.time()
+for doc in docs:
+    db.delete(doc['_id'], rev=doc['_rev'])
+elapsed = time.time() - start
+print('Seconds: {:.2f}'.format(elapsed))
+print('Deletes per second: {:.1f}'.format(count / elapsed))
 print('')
