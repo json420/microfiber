42d67a1b54ac86deac8a54f17a223b24af5fef17
Jason Gerard DeRose <jderose@novacut.com>
2011-03-27 17:14:39 -0600

    Some tweaks to the benchmark scripts

--- benchmark_microfiber.py
+++ benchmark_microfiber.py
@@ -4,11 +4,13 @@ import os
 from base64 import b32encode
 import time
 import platform
+import json
 
 import microfiber
 
 name = 'test_benchmark_microfiber'
-count = 1000
+count = 2000
+keys = 50
 
 s = microfiber.Server()
 try:
@@ -23,17 +25,18 @@ def random_id():
 
 
 master = dict(
-    ('a' * i, 'b' * i) for i in range(1, 50)
+    ('a' * i, 'b' * i) for i in range(1, keys)
 )
 ids = tuple(random_id() for i in range(count))
 docs = []
+total = 0
 
 print('*** Benchmarking microfiber ***')
 print('Python: {}, {}, {}'.format(
     platform.python_version(), platform.machine(), platform.system())
 )
 
-print('Saving {} documents in db {!r}'.format(count, name))
+print('  Saving {} documents in db {!r}...'.format(count, name))
 start = time.time()
 for _id in ids:
     doc = dict(master)
@@ -41,24 +44,28 @@ for _id in ids:
     db.save(doc)
     docs.append(doc)
 elapsed = time.time() - start
-print('Seconds: {:.2f}'.format(elapsed))
-print('Saves per second: {:.1f}'.format(count / elapsed))
-print('')
+total += elapsed
+print('    Seconds: {:.2f}'.format(elapsed))
+print('    Saves per second: {:.1f}'.format(count / elapsed))
 
-print('Getting {} documents from db {!r}'.format(count, name))
+print('  Getting {} documents from db {!r}...'.format(count, name))
 start = time.time()
 for _id in ids:
     db.get(_id)
 elapsed = time.time() - start
-print('Seconds: {:.2f}'.format(elapsed))
-print('Gets per second: {:.1f}'.format(count / elapsed))
-print('')
+total += elapsed
+print('    Seconds: {:.2f}'.format(elapsed))
+print('    Gets per second: {:.1f}'.format(count / elapsed))
 
-print('Deleting {} documents from db {!r}'.format(count, name))
+print('  Deleting {} documents from db {!r}...'.format(count, name))
 start = time.time()
 for doc in docs:
     db.delete(doc['_id'], rev=doc['_rev'])
 elapsed = time.time() - start
-print('Seconds: {:.2f}'.format(elapsed))
-print('Deletes per second: {:.1f}'.format(count / elapsed))
+total += elapsed
+print('    Seconds: {:.2f}'.format(elapsed))
+print('    Deletes per second: {:.1f}'.format(count / elapsed))
+
+print('Total seconds: {:.2f}'.format(total))
+print('Total ops per second: {:.1f}'.format((count * 3) / total))
 print('')
