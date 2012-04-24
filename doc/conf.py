3dfb43b77c3e1578571d3e458c51fbdc3f2ad1f0
Jason Gerard DeRose <jderose@novacut.com>
2012-04-23 21:53:02 -0600

    Bumped version to 12.05.0

--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ copyright = u'2011, Novacut Inc'
 # built documents.
 #
 # The short X.Y version.
-version = '12.03'
+version = '12.04'
 # The full version, including alpha/beta/rc tags.
 release = version
 
