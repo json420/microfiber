2b66c443595d3c60d5c3dfd6accda3515bc19998
Jason Gerard DeRose <jderose@novacut.com>
2012-06-07 20:30:07 -0600

    Bumped version to 12.06

--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ copyright = u'2011, Novacut Inc'
 # built documents.
 #
 # The short X.Y version.
-version = '12.04'
+version = '12.06'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- microfiber.py
+++ microfiber.py
@@ -73,7 +73,7 @@ __all__ = (
     'ServerError',
 )
 
-__version__ = '12.05.0'
+__version__ = '12.06.0'
 USER_AGENT = 'microfiber ' + __version__
 SERVER = 'http://localhost:5984/'
 DC3_CMD = ('/usr/bin/dc3', 'GetEnv')
