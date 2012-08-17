1c373515bb878c1e67f10a0db0eeb9ec2edfe5af
Jason Gerard DeRose <jderose@novacut.com>
2012-08-16 20:05:14 -0600

    Spruced up benchmark_json.py to show off the horrible performance hit of ensure_ascii=False

--- benchmark_json.py
+++ benchmark_json.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 
 import json
-import io
 import platform
 import time
 
-count = 10000
+count = 20000
 keys = 100
 
 obj = dict(
     ('a' * i, 'b' * i) for i in range(1, keys + 1)
 )
-
 
 print('*** Benchmarking json ***')
 print('Python: {}, {}, {}'.format(
@@ -19,77 +17,57 @@ print('Python: {}, {}, {}'.format(
 )
 print('Iterations: {}'.format(count))
 print('JSON encoded object size: {} bytes'.format(
-    len(json.dumps(obj, separators=(',',':')).encode('utf-8'))
+    len(json.dumps(obj).encode('utf-8'))
 ))
-print('')
-
-print('json.dump() with io.StringIO')
-start = time.time()
-for i in range(count):
-    fp = io.StringIO()
-    json.dump(obj, fp, separators=(',',':'))
-elapsed = time.time() - start
-print('  Objects per second: {:.1f}'.format(count / elapsed))
-print('')
-
-print('json.dumps()')
-start = time.time()
-for i in range(count):
-    json.dumps(obj, separators=(',',':'))
-elapsed = time.time() - start
-print('  Objects per second: {:.1f}'.format(count / elapsed))
 print('')
 
 print("json.dumps() + encode('utf-8')")
 start = time.time()
 for i in range(count):
-    json.dumps(obj, separators=(',',':')).encode('utf-8')
+    json.dumps(obj).encode('utf-8')
 elapsed = time.time() - start
-print('  Objects per second: {:.1f}'.format(count / elapsed))
+print('  Objects per second: {:.0f}'.format(count / elapsed))
+print('')
+
+print('json.dumps()')
+start = time.time()
+for i in range(count):
+    json.dumps(obj)
+elapsed = time.time() - start
+print('  Objects per second: {:.0f}'.format(count / elapsed))
 print('')
 
 print('json.dumps(sort_keys=True)')
 start = time.time()
 for i in range(count):
-    json.dumps(obj, sort_keys=True, separators=(',',':'))
+    json.dumps(obj, sort_keys=True)
 elapsed = time.time() - start
-print('  Objects per second: {:.1f}'.format(count / elapsed))
+print('  Objects per second: {:.0f}'.format(count / elapsed))
 print('')
 
-print("json.dumps(sort_keys=True) + encode('utf-8')")
+print('json.dumps(ensure_ascii=False)')
 start = time.time()
 for i in range(count):
-    json.dumps(obj, sort_keys=True, separators=(',',':')).encode('utf-8')
+    json.dumps(obj, ensure_ascii=False)
 elapsed = time.time() - start
-print('  Objects per second: {:.1f}'.format(count / elapsed))
-print('')
-
-print('json.load()  with io.StringIO')
-s = json.dumps(obj, sort_keys=True, separators=(',',':'))
-fp = io.StringIO()
-fp.write(s)
-start = time.time()
-for i in range(count):
-    fp.seek(0)
-    json.load(fp)
-elapsed = time.time() - start
-print('  Objects per second: {:.1f}'.format(count / elapsed))
-print('')
-
-print('json.loads()')
-s = json.dumps(obj, sort_keys=True, separators=(',',':'))
-start = time.time()
-for i in range(count):
-    json.loads(s)
-elapsed = time.time() - start
-print('  Objects per second: {:.1f}'.format(count / elapsed))
+print('  Objects per second: {:.0f}'.format(count / elapsed))
 print('')
 
 print("decode('utf-8') + json.loads()")
-b = json.dumps(obj, sort_keys=True, separators=(',',':')).encode('utf-8')
+b = json.dumps(obj, sort_keys=True).encode('utf-8')
 start = time.time()
 for i in range(count):
     json.loads(b.decode('utf-8'))
 elapsed = time.time() - start
-print('  Objects per second: {:.1f}'.format(count / elapsed))
+print('  Objects per second: {:.0f}'.format(count / elapsed))
 print('')
+
+print('json.loads()')
+s = json.dumps(obj, sort_keys=True)
+start = time.time()
+for i in range(count):
+    json.loads(s)
+elapsed = time.time() - start
+print('  Objects per second: {:.0f}'.format(count / elapsed))
+print('')
+
