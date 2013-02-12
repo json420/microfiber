223c1645d65a1a35c91f3ab1554ff647c9fdf4f7
Jason Gerard DeRose <jderose@novacut.com>
2013-02-12 01:33:52 -0700

    setup.py now requires Python >= 3.3

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ Install `microfiber`.
 """
 
 import sys
-if sys.version_info < (3, 2):
-    sys.exit('Microfiber requires Python 3.2 or newer')
+if sys.version_info < (3, 3):
+    sys.exit('Microfiber requires Python 3.3 or newer')
 
 from distutils.core import setup
 from distutils.cmd import Command
