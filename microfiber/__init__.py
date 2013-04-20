7a70244534e9f21e4ff96922a845452cc0984d90
Jason Gerard DeRose <jderose@novacut.com>
2013-04-19 19:16:02 -0600

    Bumped version to 13.05

--- microfiber/__init__.py
+++ microfiber/__init__.py
@@ -85,7 +85,7 @@ __all__ = (
     'ServerError',
 )
 
-__version__ = '13.04.0'
+__version__ = '13.05.0'
 log = logging.getLogger()
 USER_AGENT = 'Microfiber/{} ({} {}; {})'.format(__version__, 
     platform.dist()[0], platform.dist()[1], platform.machine()
